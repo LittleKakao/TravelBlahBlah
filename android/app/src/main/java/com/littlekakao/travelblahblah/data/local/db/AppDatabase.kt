5517944ee61f07eadb685c90ddb59a399fd0b58c
YeonHoLee-dev <yeonholee.dev@gmail.com>
2023-11-06 01:09:57 +0900

    style: Add local data files

---
+++ android/app/src/main/java/com/littlekakao/travelblahblah/data/local/db/AppDatabase.kt
@@ -0,0 +1,22 @@
+package com.littlekakao.travelblahblah.data.local.db
+
+//import androidx.room.Database
+//import androidx.room.RoomDatabase
+import com.littlekakao.travelblahblah.data.local.db.dao.CategoryDao
+import com.littlekakao.travelblahblah.data.local.db.dao.JourneyDao
+import com.littlekakao.travelblahblah.data.local.db.dao.ScheduleDao
+import com.littlekakao.travelblahblah.data.local.db.dao.UserDao
+import com.littlekakao.travelblahblah.data.model.db.Category
+import com.littlekakao.travelblahblah.data.model.db.Journey
+import com.littlekakao.travelblahblah.data.model.db.Schedule
+import com.littlekakao.travelblahblah.data.model.db.User
+
+
+//@Database(entities = {Category.class, Journey.class, Schedule.class, User.class}, version = 1)
+//abstract class AppDatabase: RoomDatabase {
+abstract class AppDatabase {
+//    abstract CategoryDao cagegoryDao()
+//    abstract JourneyDao journeyDao()
+//    abstract ScheduleDao scheduleDao()
+//    abstract UserDao userDao()
+}
