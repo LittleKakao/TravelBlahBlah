1922fd3565406c800f2645215a8f5d4db25f8d1b
YeonHoLee-dev <yeonholee.dev@gmail.com>
2023-11-14 00:23:27 +0900

    chore: Add base files

---
+++ android/app/src/main/java/com/littlekakao/travelblahblah/ui/base/BaseViewHolder.kt
@@ -0,0 +1,13 @@
+package com.littlekakao.travelblahblah.ui.base
+
+//import androidx.recyclerview.widget.RecyclerView;
+//import android.view.View
+//
+//
+//abstract class BaseViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+//    abstract fun onBind(position: Int)
+//}
+
+abstract class BaseViewHolder {
+
+}
