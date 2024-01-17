ae34f970cffa27b895367469db41015c23b3a8cc
YeonHoLee-dev <yeonholee.dev@gmail.com>
2024-01-18 00:24:46 +0900

    feat: Add entity files

--- android/app/src/main/java/com/littlekakao/travelblahblah/data/model/db/Category.kt
+++ android/app/src/main/java/com/littlekakao/travelblahblah/data/model/db/Category.kt
@@ -1,4 +1,28 @@
 package com.littlekakao.travelblahblah.data.model.db
 
-class Category {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Category (
+    val userId: Int = 0,
+    val categoryName1: String = "",
+    val categoryName2: String = "",
+    val categoryName3: String = "",
+    val categoryName4: String = "",
+    val categoryName5: String = "",
+    val categoryName6: String = "",
+    val categoryName7: String = "",
+    val categoryName8: String = "",
+    val categoryName9: String = "",
+    val categoryName10: String = "",
+    val categoryColor1: String = "",
+    val categoryColor2: String = "",
+    val categoryColor3: String = "",
+    val categoryColor4: String = "",
+    val categoryColor5: String = "",
+    val categoryColor6: String = "",
+    val categoryColor7: String = "",
+    val categoryColor8: String = "",
+    val categoryColor9: String = "",
+    val categoryColor10: String = ""
+)
